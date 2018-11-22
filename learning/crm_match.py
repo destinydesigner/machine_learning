a2e5b6afdc83f4c19461018e90c54aa9ecd689e5
Fang Guojian <gajanfang@gmail.com>
2018-11-23 03:06:05 +0800

    analyze false positive and false negative

--- learning/crm_match.py
+++ learning/crm_match.py
@@ -14,15 +14,17 @@ df = df[df.distance < 1000]
 data = df[['name', 'address', 'distance']]
 label = df['matched']
 
-training_data, test_data = data[0:200000].values, data[200000:].values
-training_label, test_label = label[0:200000].values, label[200000:].values
+training_data_size = 150000
+
+training_data, test_data = data[0:training_data_size].values, data[training_data_size:].values
+training_label, test_label = label[0:training_data_size].values, label[training_data_size:].values
 
 print(training_data.shape)
 print(training_data, training_label)
 
 
 model = keras.Sequential([
-    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(3, activation=tf.nn.relu),
     keras.layers.Dense(2, activation=tf.nn.softmax)
 ])
 
@@ -41,20 +43,36 @@ print(predictions)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-correct = []
-wrong = []
+positive = []
+negative = []
+true_positive = []
+true_negative = []
+false_positive = []
+false_negative = []
 for i in range(0, len(test_data), 1):
     prediction_array = predictions[i]
     result = np.argmax(prediction_array)
-
-    if result == test_label[i]:
-        correct.append(test_data[i])
+    d = test_data[i]
+    if test_label[i]:
+        positive.append(d)
+        if result:
+            true_positive.append(d)
+        else:
+            false_negative.append(d)
     else:
-        wrong.append(test_data[i])
+        negative.append(d)
+        if result:
+            false_positive.append(d)
+        else:
+            true_negative.append(d)
 
-print( 1.0 * len(wrong) / (len(wrong) + len(correct)))
-#ax.scatter(*zip(*correct), color='blue', marker='^', s=1)
-ax.scatter(*zip(*wrong), color='red', marker='o', s=2)
+print( 1.0 * len(negative) / (len(negative) + len(positive)))
+# ax.scatter(*zip(*positive), color='blue', marker='^', s=2)
+# ax.scatter(*zip(*negative), color='red', marker='o', s=2)
+# ax.scatter(*zip(*true_positive), color='red', marker='o', s=2)
+# ax.scatter(*zip(*true_negative), color='red', marker='o', s=2)
+# ax.scatter(*zip(*false_positive), color='red', marker='o', s=2)
+ax.scatter(*zip(*false_negative), color='red', marker='o', s=2)
 print('done')
 
 ax.set_xlabel('name')
